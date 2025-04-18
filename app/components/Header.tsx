1f32fc5484fd9b3a682de6f42db2add4c9d93b13
Pak <paulo212@hotmail.ch>
2025-04-18 18:48:14 +0200

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -21,6 +21,8 @@ export default function Header() {
           <Link href="/whitepaper" className="hover:text-zinc-400 transition">Whitepaper</Link>
           <Link href="/#footer" className="hover:text-zinc-400 transition">Contact</Link>
         </nav>
+
+        <div className="hidden md:flex space-x-4 text-sm">
         </div>
 
         {/* Burger Icon for Mobile */}
@@ -45,9 +47,6 @@ export default function Header() {
               <Link href="/#footer" onClick={toggleMobile}>Contact</Link>
             </nav>
             <div className="flex space-x-4 mt-4 text-sm">
-              <a href="https://x.com/Thedump25" target="_blank">X</a>
-              <a href="https://t.me/+kdThjHWnV-JjZDY8" target="_blank">Telegram</a>
-              <a href="https://github.com/PAKmeme" target="_blank">GitHub</a>
             </div>
           </motion.div>
         )}
