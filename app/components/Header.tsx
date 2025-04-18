5237ba1b22fc151462d6ee0fa2783d5d4411b03f
Pak <paulo212@hotmail.ch>
2025-04-18 16:32:30 +0200

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,5 @@
-// app/components/Header.tsx
+// ✅ Navigation Header for DUMP25 site
+// Updated: 'Contact' link points to the footer
 
 import Link from 'next/link';
 
@@ -14,16 +15,15 @@ export default function Header() {
           <Link href="/#about" className="hover:text-zinc-400 transition">About</Link>
           <Link href="/#intro" className="hover:text-zinc-400 transition">History</Link>
           <a href="/whitepaper.pdf" target="_blank" className="hover:text-zinc-400 transition">Whitepaper</a>
-          <Link href="/#contact" className="hover:text-zinc-400 transition">Contact</Link>
+          <Link href="/#footer" className="hover:text-zinc-400 transition">Contact</Link>
         </nav>
-        <footer className="py-12 bg-black text-center text-gray-400 text-sm space-y-4">
-        <div className="flex flex-wrap justify-center gap-4 text-lg">
-          <a href="https://twitter.com/Thedump25" className="hover:text-white">X (Twitter)</a>
-          <a href="https://t.me/+kdThjHWnV-JjZDY8" className="hover:text-white">Telegram</a>
-          <a href="https://github.com/PAKmeme" className="hover:text-white">GitHub</a>
-          <a href="mailto:contact@dump25.io" className="hover:text-white">contact@dump25.io</a>
+
+        <div className="flex space-x-4 text-sm">
+          <a href="https://x.com/Thedump25" target="_blank" className="hover:text-zinc-400">X</a>
+          <a href="https://t.me/+kdThjHWnV-JjZDY8" target="_blank" className="hover:text-zinc-400">Telegram</a>
+          <a href="https://github.com/PAKmeme" target="_blank" className="hover:text-zinc-400">GitHub</a>
         </div>
-     </div>
+      </div>
     </header>
   );
 }
